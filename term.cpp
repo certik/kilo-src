1fd7ad35bd86dab98be5d651009589995b28b585
Ondřej Čertík <ondrej@certik.us>
2019-09-22 17:55:22 -0600

    Use VTIME = 0

--- term.cpp
+++ term.cpp
@@ -147,7 +147,7 @@ void enableRawMode() {
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+  raw.c_cc[VTIME] = 0;
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
