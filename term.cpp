10389cf8004027025eeed6ac21488d644eed4223
Ondřej Čertík <ondrej@certik.us>
2019-09-22 20:09:06 -0600

    Add Terminal class

--- term.cpp
+++ term.cpp
@@ -2,7 +2,9 @@
 
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
-//#define _GNU_SOURCE
+
+#include <stdexcept>
+#include <iostream>
 
 #include <ctype.h>
 #include <errno.h>
@@ -131,6 +133,35 @@ void die(const char *s) {
   perror(s);
   exit(1);
 }
+
+class Terminal {
+private:
+    struct termios orig_termios;
+public:
+    Terminal() {
+        if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+            throw std::runtime_error("tcgetattr() failed");
+        }
+
+        struct termios raw = orig_termios;
+        raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.c_oflag &= ~(OPOST);
+        raw.c_cflag |= (CS8);
+        raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+        raw.c_cc[VMIN] = 0;
+        raw.c_cc[VTIME] = 0;
+
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+            throw std::runtime_error("tcsetattr() failed");
+        }
+    }
+    ~Terminal() {
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+            std::cout << "tcsetattr() failed, terminating." << std::endl;
+            exit(1);
+        }
+    }
+};
 
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
