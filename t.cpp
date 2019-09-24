5af418f6b63cc6ffe962371d5f4d984490533a98
Ondřej Čertík <ondrej@certik.us>
2019-09-23 18:06:18 -0600

    Add a test

---
+++ t.cpp
@@ -0,0 +1,17 @@
+#include "terminal.h"
+
+int main() {
+  // We must put all code in try/catch block, otherwise destructors are not
+  // being called when exception happens and the terminal is not put into
+  // correct state.
+  try {
+      Terminal term;
+      std::cout << "OK" << "\r\n";
+      int rows, cols;
+      term.get_term_size_slow(rows, cols);
+      std::cout << "Dimension:" << cols << " " << rows << "\r\n";
+  } catch(...) {
+      throw;
+  }
+  return 0;
+}
