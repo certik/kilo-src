6f3770a339588623cc486dc6c426734090cd7a3a
Ondřej Čertík <ondrej@certik.us>
2019-09-23 18:57:25 -0600

    repl

--- t.cpp
+++ t.cpp
@@ -1,17 +1,20 @@
 #include "terminal.h"
 
 int main() {
-  // We must put all code in try/catch block, otherwise destructors are not
-  // being called when exception happens and the terminal is not put into
-  // correct state.
-  try {
-      Terminal term;
-      std::cout << "OK" << std::endl;;
-      int rows, cols;
-      term.get_term_size(rows, cols);
-      std::cout << "Dimension:" << cols << " " << rows << std::endl;
-  } catch(...) {
-      throw;
-  }
-  return 0;
+    try {
+        Terminal term;
+        std::cout << "OK" << std::endl;;
+        int rows, cols;
+        term.get_term_size(rows, cols);
+        std::cout << "Dimension:" << cols << " " << rows << std::endl;
+        std::cout << "Press any key:" << std::endl;
+        while (true) {
+            int key = term.read_key();
+            std::cout << "Got:" << key << std::endl;
+            if (key == 'q') break;
+        }
+        } catch(...) {
+            throw;
+        }
+    return 0;
 }
